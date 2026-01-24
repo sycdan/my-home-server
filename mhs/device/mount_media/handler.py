fee725e53c98b3103971342f015ae0ac1157596a
Dan Stace <dstace@gmail.com>
2026-01-24 15:28:19 -0500

    skip devices with no mounts

--- mhs/device/mount_media/handler.py
+++ mhs/device/mount_media/handler.py
@@ -6,7 +6,7 @@ def handle():
   media = load_media.index()
   servers = load_devices.index()
   for server in servers.values():
-    if not server.services:
+    if not server.mounts:
       continue
     for storage_key in server.mounts:
       storage = media.get(storage_key)
